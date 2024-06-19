d58163e6ae95da5d04fbf623f488cfb5a96fa464
Ali <garudamon.letter@gmail.com>
2024-06-19 14:09:54 +0700

    js logic

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-console.log("Hello world!");
+var isSpeaking = false;
+
+const speak = (char) => {
+  isSpeaking = true;
+
+  const utterance = new SpeechSynthesisUtterance(`${char}!`);
+  utterance.lang = "ID";
+  utterance.pitch = 0.8;
+  utterance.rate = 0;
+  speechSynthesis.speak(utterance);
+
+  setTimeout(() => {
+    isSpeaking = false;
+  }, 1000);
+};
+
+const readInput = (ev) => {
+  let keyboardInput = String.fromCharCode(ev.keyCode);
+  if (keyboardInput.match(/(\w|\s)/g) && !isSpeaking) {
+    document.getElementsByTagName("h1")[0].innerText = keyboardInput;
+    speak(keyboardInput);
+  }
+};
+
+document.addEventListener("keypress", readInput);
