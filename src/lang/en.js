2c65cb313c5320be187f6d15cd8d036655d6763f
Ali <garudamon.letter@gmail.com>
2025-06-08 22:25:23 +0700

    change image to emoji

--- src/lang/en.js
+++ src/lang/en.js
@@ -1,137 +1,28 @@
-const en = [
-  {
-    huruf: "A",
-    nama: "Aardvark",
-    gambar:
-      "https://a-z-animals.com/media/animals/images/original/aardvark.jpg",
-  },
-  {
-    huruf: "B",
-    nama: "Baboon",
-    gambar: "https://a-z-animals.com/media/animals/images/original/baboon.jpg",
-  },
-  {
-    huruf: "C",
-    nama: "Cheetah",
-    gambar: "https://a-z-animals.com/media/animals/images/original/cheetah.jpg",
-  },
-  {
-    huruf: "D",
-    nama: "Dolphin",
-    gambar: "https://a-z-animals.com/media/animals/images/original/dolphin.jpg",
-  },
-  {
-    huruf: "E",
-    nama: "Elephant",
-    gambar:
-      "https://a-z-animals.com/media/animals/images/original/elephant.jpg",
-  },
-  {
-    huruf: "F",
-    nama: "Falcon",
-    gambar: "https://a-z-animals.com/media/animals/images/original/falcon.jpg",
-  },
-  {
-    huruf: "G",
-    nama: "Giraffe",
-    gambar: "https://a-z-animals.com/media/animals/images/original/giraffe.jpg",
-  },
-  {
-    huruf: "H",
-    nama: "Hippopotamus",
-    gambar:
-      "https://a-z-animals.com/media/animals/images/original/hippopotamus.jpg",
-  },
-  {
-    huruf: "I",
-    nama: "Iguana",
-    gambar: "https://a-z-animals.com/media/animals/images/original/iguana.jpg",
-  },
-  {
-    huruf: "J",
-    nama: "Jaguar",
-    gambar: "https://a-z-animals.com/media/animals/images/original/jaguar.jpg",
-  },
-  {
-    huruf: "K",
-    nama: "Kangaroo",
-    gambar:
-      "https://a-z-animals.com/media/animals/images/original/kangaroo.jpg",
-  },
-  {
-    huruf: "L",
-    nama: "Lion",
-    gambar: "https://a-z-animals.com/media/animals/images/original/lion.jpg",
-  },
-  {
-    huruf: "M",
-    nama: "Meerkat",
-    gambar: "https://a-z-animals.com/media/animals/images/original/meerkat.jpg",
-  },
-  {
-    huruf: "N",
-    nama: "Narwhal",
-    gambar: "https://a-z-animals.com/media/animals/images/original/narwhal.jpg",
-  },
-  {
-    huruf: "O",
-    nama: "Octopus",
-    gambar: "https://a-z-animals.com/media/animals/images/original/octopus.jpg",
-  },
-  {
-    huruf: "P",
-    nama: "Penguin",
-    gambar: "https://a-z-animals.com/media/animals/images/original/penguin.jpg",
-  },
-  {
-    huruf: "Q",
-    nama: "Quokka",
-    gambar: "https://a-z-animals.com/media/animals/images/original/quokka.jpg",
-  },
-  {
-    huruf: "R",
-    nama: "Rhinoceros",
-    gambar:
-      "https://a-z-animals.com/media/animals/images/original/rhinoceros.jpg",
-  },
-  {
-    huruf: "S",
-    nama: "Sloth",
-    gambar: "https://a-z-animals.com/media/animals/images/original/sloth.jpg",
-  },
-  {
-    huruf: "T",
-    nama: "Tiger",
-    gambar: "https://a-z-animals.com/media/animals/images/original/tiger.jpg",
-  },
-  {
-    huruf: "U",
-    nama: "Urial",
-    gambar: "https://a-z-animals.com/media/animals/images/original/urial.jpg",
-  },
-  {
-    huruf: "V",
-    nama: "Vulture",
-    gambar: "https://a-z-animals.com/media/animals/images/original/vulture.jpg",
-  },
-  {
-    huruf: "W",
-    nama: "Walrus",
-    gambar: "https://a-z-animals.com/media/animals/images/original/walrus.jpg",
-  },
-  {
-    huruf: "X",
-    nama: "Xerus",
-    gambar: "https://a-z-animals.com/media/animals/images/original/xerus.jpg",
-  },
-  {
-    huruf: "Y",
-    nama: "Yak",
-    gambar: "https://a-z-animals.com/media/animals/images/original/yak.jpg",
-  },
-  {
-    huruf: "Z",
-    nama: "Zebra",
-    gambar: "https://a-z-animals.com/media/animals/images/original/zebra.jpg",
-  },
+export default [
+  { huruf: "A", nama: "Ant", emoji: "🐜" },
+  { huruf: "B", nama: "Baboon", emoji: "🐒" },
+  { huruf: "C", nama: "Cheetah", emoji: "🐆" },
+  { huruf: "D", nama: "Dolphin", emoji: "🐬" },
+  { huruf: "E", nama: "Elephant", emoji: "🐘" },
+  { huruf: "F", nama: "Falcon", emoji: "🦅" },
+  { huruf: "G", nama: "Giraffe", emoji: "🦒" },
+  { huruf: "H", nama: "Hippopotamus", emoji: "🦛" },
+  { huruf: "I", nama: "Iguana", emoji: "🦎" },
+  { huruf: "J", nama: "Jaguar", emoji: "🐆" },
+  { huruf: "K", nama: "Kangaroo", emoji: "🦘" },
+  { huruf: "L", nama: "Lion", emoji: "🦁" },
+  { huruf: "M", nama: "Meerkat", emoji: "🐿️" },
+  { huruf: "N", nama: "Narwhal", emoji: "🐋" },
+  { huruf: "O", nama: "Octopus", emoji: "🐙" },
+  { huruf: "P", nama: "Penguin", emoji: "🐧" },
+  { huruf: "Q", nama: "Quokka", emoji: "🦡" }, // mendekati
+  { huruf: "R", nama: "Rhinoceros", emoji: "🦏" },
+  { huruf: "S", nama: "Sloth", emoji: "🦥" },
+  { huruf: "T", nama: "Tiger", emoji: "🐅" },
+  { huruf: "U", nama: "Urial", emoji: "🐏" },
+  { huruf: "V", nama: "Vulture", emoji: "🦅" },
+  { huruf: "W", nama: "Walrus", emoji: "🦭" },
+  { huruf: "X", nama: "Xerus", emoji: "🐿️" }, // mendekati tupai
+  { huruf: "Y", nama: "Yak", emoji: "🐂" },
+  { huruf: "Z", nama: "Zebra", emoji: "🦓" },
 ];
--- src/lang/id.js
+++ src/lang/id.js
@@ -1,132 +1,28 @@
-const id = [
-  {
-    huruf: "A",
-    nama: "Angsa",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/angsa.jpg",
-  },
-  {
-    huruf: "B",
-    nama: "Badak",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/badak.jpg",
-  },
-  {
-    huruf: "C",
-    nama: "Cacing",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/cacing.jpg",
-  },
-  {
-    huruf: "D",
-    nama: "Domba",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/domba.jpg",
-  },
-  {
-    huruf: "E",
-    nama: "Elang",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/elang.jpg",
-  },
-  {
-    huruf: "F",
-    nama: "Flamingo",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/flamingo.jpg",
-  },
-  {
-    huruf: "G",
-    nama: "Gajah",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/gajah.jpg",
-  },
-  {
-    huruf: "H",
-    nama: "Harimau",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/harimau.jpg",
-  },
-  {
-    huruf: "I",
-    nama: "Ikan",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/ikan.jpg",
-  },
-  {
-    huruf: "J",
-    nama: "Jerapah",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/jerapah.jpg",
-  },
-  {
-    huruf: "K",
-    nama: "Kucing",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/kucing.jpg",
-  },
-  {
-    huruf: "L",
-    nama: "Lumba-lumba",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/lumba-lumba.jpg",
-  },
-  {
-    huruf: "M",
-    nama: "Macan tutul",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/macan-tutul.jpg",
-  },
-  {
-    huruf: "N",
-    nama: "Nyamuk",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/nyamuk.jpg",
-  },
-  {
-    huruf: "O",
-    nama: "Orangutan",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/orangutan.jpg",
-  },
-  {
-    huruf: "P",
-    nama: "Penguin",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/penguin.jpg",
-  },
-  {
-    huruf: "Q",
-    nama: "Quokka",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/quokka.jpg",
-  },
-  {
-    huruf: "R",
-    nama: "Rusa",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/rusa.jpg",
-  },
-  {
-    huruf: "S",
-    nama: "Singa",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/singa.jpg",
-  },
-  {
-    huruf: "T",
-    nama: "Trenggiling",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/trenggiling.jpg",
-  },
-  {
-    huruf: "U",
-    nama: "Ular",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/ular.jpg",
-  },
-  {
-    huruf: "V",
-    nama: "Viper",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/viper.jpg",
-  },
-  {
-    huruf: "W",
-    nama: "Walrus",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/walrus.jpg",
-  },
-  {
-    huruf: "X",
-    nama: "Xantus",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/xantus.jpg",
-  },
-  {
-    huruf: "Y",
-    nama: "Yak",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/yak.jpg",
-  },
-  {
-    huruf: "Z",
-    nama: "Zebra",
-    gambar: "https://rimbakita.com/wp-content/uploads/2020/09/zebra.jpg",
-  },
+export default [
+  { huruf: "A", nama: "Angsa", emoji: "🦢" },
+  { huruf: "B", nama: "Badak", emoji: "🦏" },
+  { huruf: "C", nama: "Cacing", emoji: "🪱" },
+  { huruf: "D", nama: "Domba", emoji: "🐑" },
+  { huruf: "E", nama: "Elang", emoji: "🦅" },
+  { huruf: "F", nama: "Flamingo", emoji: "🦩" },
+  { huruf: "G", nama: "Gajah", emoji: "🐘" },
+  { huruf: "H", nama: "Harimau", emoji: "🐅" },
+  { huruf: "I", nama: "Ikan", emoji: "🐟" },
+  { huruf: "J", nama: "Jerapah", emoji: "🦒" },
+  { huruf: "K", nama: "Kucing", emoji: "🐱" },
+  { huruf: "L", nama: "Lumba-lumba", emoji: "🐬" },
+  { huruf: "M", nama: "Macan tutul", emoji: "🐆" },
+  { huruf: "N", nama: "Nyamuk", emoji: "🦟" },
+  { huruf: "O", nama: "Orangutan", emoji: "🦧" },
+  { huruf: "P", nama: "Penguin", emoji: "🐧" },
+  { huruf: "Q", nama: "Quokka", emoji: "🦡" }, // paling mendekati
+  { huruf: "R", nama: "Rusa", emoji: "🦌" },
+  { huruf: "S", nama: "Singa", emoji: "🦁" },
+  { huruf: "T", nama: "Trenggiling", emoji: "🐾" }, // tidak ada emoji khusus
+  { huruf: "U", nama: "Ular", emoji: "🐍" },
+  { huruf: "V", nama: "Viper", emoji: "🐍" }, // sama dengan ular
+  { huruf: "W", nama: "Walrus", emoji: "🦭" },
+  { huruf: "X", nama: "Xantus", emoji: "🐦" }, // burung secara umum
+  { huruf: "Y", nama: "Yak", emoji: "🐂" },
+  { huruf: "Z", nama: "Zebra", emoji: "🦓" },
 ];
